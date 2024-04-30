6d2bc89f2e19e4fa521e5858f39baf4333de27e0
hammed Akinwale <hammedakinwale.com>
2024-04-30 06:49:56 +0100

    updating pipeline

--- src/test/java/integration/ninjas/HandlerStringTest.java
+++ src/test/java/integration/ninjas/HandlerStringTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class HandlerString
+public class HandlerStringTest
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class HandlerString
      *
      * @param testName name of the test case
      */
-    public HandlerString(String testName )
+    public HandlerStringTest(String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class HandlerString
      */
     public static Test suite()
     {
-        return new TestSuite( HandlerString.class );
+        return new TestSuite( HandlerStringTest.class );
     }
 
     /**
